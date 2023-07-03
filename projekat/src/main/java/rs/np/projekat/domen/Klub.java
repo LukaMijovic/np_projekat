bbd7a30d92cbbbef4d9d7e3ff8f074ac32c4f614
Luka Mijovic <mijovic.luka@gmail.com>
2023-07-03 20:57:42 +0200

    Kreirana klasa Klub sa svojim javadocom.

---
+++ projekat/src/main/java/rs/np/projekat/domen/Klub.java
@@ -0,0 +1,243 @@
+package rs.np.projekat.domen;
+
+/**
+ * Klasa Klub predstavlja jedan klub koji moze da odigra utakmicu i ima svoje igrace.
+ * 
+ * @author mijov
+ */
+public class Klub {
+
+	/**
+	 * Id kluba kao int.
+	 */
+	private int id;
+	
+	/**
+	 * Naziv kluba kao string.
+	 */
+	private String naziv;
+	
+	/**
+	 * Broj pobeda kluba.
+	 */
+	private int brojW;
+	
+	/**
+	 * Broj poraza kluba.
+	 */
+	private int brojL;
+	
+	/**
+	 * Broj neresenih rezultata kluba.
+	 */
+	private int brojD;
+	
+	/**
+	 * Id stadiona na kom klub igra.
+	 */
+	private int stadion;
+	
+	/**
+	 * Id trenera kluba.
+	 */
+	private int trener;
+	
+	/**
+	 * Drzava iz koje je klub.
+	 */
+	private int drzava;
+
+	public Klub(int id, String naziv, int brojW, int brojL, int brojD, int stadion, int trener, int drzava) {
+		super();
+		this.id = id;
+		this.naziv = naziv;
+		this.brojW = brojW;
+		this.brojL = brojL;
+		this.brojD = brojD;
+		this.stadion = stadion;
+		this.trener = trener;
+		this.drzava = drzava;
+	}
+
+	public Klub() {
+		super();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * Postavlja id kluba.
+	 * 
+	 * @param id
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNaziv() {
+		return naziv;
+	}
+
+	/**
+	 * Postavlja naziv kluba.
+	 * 
+	 * @param naziv
+	 * @throws IllegalArgumentException kada je naziv kluba null ili prazan string.
+	 */
+	public void setNaziv(String naziv) throws IllegalArgumentException {
+		if (naziv == null || naziv.length() == 0) {
+			throw new IllegalArgumentException("Naziv kluba ne moze biti null ili prazan string");
+		}
+		
+		this.naziv = naziv;
+	}
+
+	public int getBrojW() {
+		return brojW;
+	}
+
+	/**
+	 * Postavlja broj pobeda.
+	 * 
+	 * @param brojW
+	 * @throws IllegalArgumentException kada je broj pobeda negativan.
+	 */
+	public void setBrojW(int brojW) throws IllegalArgumentException {
+		if (brojW < 0) {
+			throw new IllegalArgumentException("Broj pobeda ne moze biti negativan broj.");
+		}
+		
+		this.brojW = brojW;
+	}
+
+	public int getBrojL() {
+		return brojL;
+	}
+
+	/**
+	 * Postavlja broj poraza.
+	 * 
+	 * @param brojL
+	 * @throws IllegalArgumentException kada je broj poraza negativan.
+	 */
+	public void setBrojL(int brojL) throws IllegalArgumentException {
+		if (brojL < 0) {
+			throw new IllegalArgumentException("Broj poraza ne moze biti negativan broj.");
+		}
+		
+		this.brojL = brojL;
+	}
+
+	public int getBrojD() {
+		return brojD;
+	}
+
+	/**
+	 * Postavlja broj neresenih rezultata.
+	 * 
+	 * @param brojD
+	 * @throws IllegalArgumentException kada je broj neresenih rezultata negativan.
+	 */
+	public void setBrojD(int brojD) throws IllegalArgumentException {
+		if (brojD < 0) {
+			throw new IllegalArgumentException("Broj neresenih rezulata ne moze biti negativan broj.");
+		}
+		
+		this.brojD = brojD;
+	}
+
+	public int getStadion() {
+		return stadion;
+	}
+
+	/**
+	 * Postavlja id stadiona na kom klub igra.
+	 * 
+	 * @param stadion
+	 * @throws IllegalArgumentException kad je id kluba negativan.
+	 */
+	public void setStadion(int stadion) throws IllegalArgumentException {
+		if (stadion < 0) {
+			throw new IllegalArgumentException("Id stadiona na kom klub igra ne moze biti negativan.");
+		}
+		
+		this.stadion = stadion;
+	}
+
+	public int getTrener() {
+		return trener;
+	}
+
+	
+	/**
+	 * Postavlja id trenera koji trenira klub.
+	 * 
+	 * @param trener
+	 * @throws IllegalArgumentException kada je id trenera negativan.
+	 */
+	public void setTrener(int trener) throws IllegalArgumentException {
+		if (trener < 0) {
+			throw new IllegalArgumentException("Id trenera koji trenira klub ne moze biti negativan.");
+		}
+		
+		this.trener = trener;
+	}
+
+	public int getDrzava() {
+		return drzava;
+	}
+
+	/**
+	 * Id drzave iz koje je klub.
+	 * 
+	 * @param drzava
+	 * @throws IllegalArgumentException kada je id drzave negativan.
+	 */
+	public void setDrzava(int drzava) throws IllegalArgumentException {
+		if (drzava < 0) {
+			throw new IllegalArgumentException("Id drzave ne moze biti negativan.");
+		}
+		
+		this.drzava = drzava;
+	}
+
+	/**
+	 * Override metode hashCode klase Object koji kreira hashcode na osnovu id.
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	/**
+	 * Poredi dve instance kluba na osnovu kluba.
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Klub other = (Klub) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+	/**
+	 * Override toString metode klase Object tako sto vraca string od atributa naziv i drzava.
+	 */
+	@Override
+	public String toString() {
+		return "Klub [naziv=" + naziv + ", drzava=" + drzava + "]";
+	}
+	
+	
+}
